08915b4f854114b4fddaea07a88bfbfc1a830634
kchu01 <chukathy01@gmail.com>
2021-03-10 23:19:15 -0800

    Include fetched api

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import Villager from './component/Villager';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Villager />
+      </div>
+    )
+  }
+}
+
