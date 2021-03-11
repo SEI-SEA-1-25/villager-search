27b1546345760487da34cef532a104db59fce3b4
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-03-10 19:02:27 -0800

    Until refactor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { Component } from 'react';
+import DisplayCards from './components/DisplayCards'
 
 class App extends Component {
+
+  state = {
+    villagers: []
+  }
 
   componentDidMount() {
     fetch('http://acnhapi.com/v1/villagers/')
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
+      this.setState({
+          villagers: Object.values(data)
+      })
     })
   }
 
   render() {
-    return(
+    return (
       <div>
-        <h1>Hello, World!</h1>
+        {this.state.villagers.map((villager) => (
+          <p>{villager.name['name-USen']}</p>
+        ))}
       </div>
     )
   }
