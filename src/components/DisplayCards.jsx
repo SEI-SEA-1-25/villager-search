fd33d79e4e4d02efda96f041156feba7e5962a97
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-03-10 19:46:54 -0800

    mvp editted

--- src/components/DisplayCards.jsx
+++ src/components/DisplayCards.jsx
@@ -4,7 +4,6 @@ class DisplayCards extends Component {
     render() {
         return (
             <div>
-                <h1>{this.props.wallah}</h1>
                 {this.props.villagers.map((villager, i) => (
                     <div key={i}>
                         <img alt={villager.name["name-USen"]} src={villager["image_uri"]} />
